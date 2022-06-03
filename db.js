4b87a40f30e508eefa3453861b0f49d04fdb04fa
Anna-de-Angelis <101907319+Anna-de-Angelis@users.noreply.github.com>
2022-06-02 22:38:38 -0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,59 @@
+var db = {
+    dados: [
+            
+   
+        {
+             
+            imagem:'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/main/docs/img/Persona1.png?raw=true',
+            nome: 'Luiz Campos',
+            instituicaoDeEnsino: 'PUC Minas',
+            cidade: 'Belo Horizonte',
+            disciplinas: 'Engenharia de software, Matemática básica',
+            contato: ''
+
+        }, {
+            imagem: href= 'C:\Users\User\Desktop\arquivostutoria\persona1.png',
+            nome: 'Vanessa Alves',
+            instituicaoDeEnsino: 'UFMG',
+            cidade: 'Belo Horizonte',
+            disciplinas: 'História da Arquitetura, Análise e Gestão Ambiental',
+            contato: ''
+
+        }, {
+            imagem: 'href=\Users\User\Desktop\arquivostutoria\persona3.png',
+            nome: 'Luciano Silva',
+            instituicaoDeEnsino: 'PUC Minas',
+            cidade: 'Belo Horizonte',
+            disciplinas: 'Direito Empresarial, Direito Civil',
+            contato: ''
+
+        }, {
+            imagem:href= 'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/cc86094ca9785c44e3aa13676d14cebc82d191b2/docs/img/Livia.png',
+            nome: 'Lívia Moreira',
+            instituicaoDeEnsino: 'UFRJ',
+            cidade: 'Rio de Janeiro',
+            disciplinas: 'Anatomia, Pediatria' ,
+            contato: ''
+
+        }, {
+            imagem: '',
+             nome: "Caio Marques",
+             instituicaoDeEnsino: 'UFOP',
+             cidade: 'Ouro Preto',
+             disciplinas: 'Engenharia de Software',
+             contato: ''
+
+        }, {
+             imagem: 'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/3c38c0715167350a541166b329e9f3bc9aa5a244/persona5.jpeg',
+             nome: "Luiza Aleixo",
+             instituicaoDeEnsino: 'UNA',
+             cidade: 'Belo Horizonte',
+             disciplinas: 'Medicina Veterinária 1, Animais Silvestres',
+             contato: ''
+
+   
+    }
+    
+    ]}
+  
+    
