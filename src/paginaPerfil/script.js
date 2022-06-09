648c4b14af4e7143fb08483c9ce447114cfaabf8
Daiane Procópio <100447878+procopiodaiane@users.noreply.github.com>
2022-06-09 12:23:08 -0300

    Add files via upload

---
+++ src/paginaPerfil/script.js
@@ -0,0 +1,57 @@
+
+
+
+function cadastrar() {
+  if (nome.value == "") {
+    alert("Preencha o formulário corretamente!");
+    nome.focus();
+    return;
+  }
+  if (instituicao.value == "") {
+    alert("Preencha o formulário corretamente!");
+    instituicao.focus;
+    return;
+  }
+  if (cidade.value == "") {
+    alert("Preencha o formulário corretamente!");
+    cidade.focus;
+    return;
+  }
+  if (disciplinas.value == "") {
+    alert("Preencha o formulário corretamente!");
+    disciplinas.focus;
+    return;
+  }
+  if (contato.value == "") {
+    alert("Preencha o formulário corretamente!");
+    contato.focus;
+    return;
+  }
+
+  let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+
+  listaUser.push(
+    {
+      nomeUser: nome.value,
+      instituicaoUser: formulario.instituicao.value,
+      cidadeUser: formulario.cidade.value,
+      disciplinasUser: formulario.disciplinas.value,
+      contatoUser: formulario.contato.value,
+    });
+
+  localStorage.setItem("listaUser", JSON.stringify(listaUser))
+
+  alert("Tutoria cadastrada com sucesso!");
+  window.location.href = "http://127.0.0.1:5500/cadastroTutorias.html"
+}
+
+
+
+
+
+
+
+
+
+
+
