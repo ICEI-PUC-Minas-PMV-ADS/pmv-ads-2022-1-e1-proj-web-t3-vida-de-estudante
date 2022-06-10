8becef6c1861038884a44af618348781acfabe4b
Daiane Procópio <100447878+procopiodaiane@users.noreply.github.com>
2022-06-10 17:26:13 -0300

    Update script2.js

--- src/paginaPerfil/script2.js
+++ src/paginaPerfil/script2.js
@@ -54,7 +54,7 @@ function cadastrar() {
   localStorage.setItem("listaUser", JSON.stringify(listaUser))
 
   alert("Livro cadastrado com sucesso!");
-  window.location.href = "http://127.0.0.1:5500/cadastroLivros.html"
+  window.location.href = "../paginaPerfil/cadastroLivros.html"
 }
 
 
