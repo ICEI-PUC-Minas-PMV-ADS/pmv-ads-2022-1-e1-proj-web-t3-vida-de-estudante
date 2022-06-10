3ecc0c943dd9c233d805f4fba647dec507854b13
Anna-de-Angelis <101907319+Anna-de-Angelis@users.noreply.github.com>
2022-06-10 20:23:03 -0300

    Add files via upload

---
+++ src/página Tutorias/db.js
@@ -0,0 +1,60 @@
+var db = {
+    dados: [
+            
+   
+        {
+             
+            imagem:'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/main/docs/img/Persona1.png?raw=true',
+            nome: 'Luiz Campos',
+            instituicaoDeEnsino: 'PUC Minas',
+            cidade: 'Belo Horizonte',
+            disciplinas: 'Engenharia de software, Matemática básica',
+            contato: 'luizcampos@pucminas.com'
+    
+
+        }, {
+            imagem: 'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/main/docs/img/persona2.png?raw=true',
+            nome: 'Vanessa Alves',
+            instituicaoDeEnsino: 'UFMG',
+            cidade: 'Belo Horizonte',
+            disciplinas: 'História da Arquitetura, Análise e Gestão Ambiental',
+            contato: 'vanessaa@ufmg.com'
+
+        }, {
+            imagem: 'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/main/docs/img/Persona3.png?raw=true',
+            nome: 'Luciano Silva',
+            instituicaoDeEnsino: 'PUC Minas',
+            cidade: 'Belo Horizonte',
+            disciplinas: 'Direito Empresarial, Direito Civil',
+            contato: 'lucianosilva55@pucminas.com'
+
+        }, {
+            imagem:'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/main/docs/img/Livia.png?raw=true',
+            nome: 'Lívia Moreira',
+            instituicaoDeEnsino: 'UFRJ',
+            cidade: 'Rio de Janeiro',
+            disciplinas: 'Anatomia, Pediatria' ,
+            contato: 'liviamoreira78@ufrj.com'
+
+        }, {
+            imagem: 'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/main/docs/img/persona5.jpeg?raw=true',
+            nome: 'Caio Marques', 
+            instituicaoDeEnsino: 'UFOP',
+            cidade: 'Ouro Preto',
+            disciplinas: 'Engenharia de Software',
+            contato: 'caiom@ufop.com'
+
+        }, {
+             imagem: 'https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-1-e1-proj-web-t3-vida-de-estudante/blob/pagina-tutorias/persona%206.jpg?raw=true',
+             nome: "Luiza Aleixo",
+             instituicaoDeEnsino: 'UNA',
+             cidade: 'Belo Horizonte',
+             disciplinas: 'Medicina Veterinária 1, Animais Silvestres',
+             contato: 'luizaaleixo98@una.com'
+
+   
+    }
+    
+    ]}
+  
+    
